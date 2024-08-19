ce66170c6287c2e33dfb35d86a0b7abf9f460101
Minakli <ges-a@ya.ru>
2024-08-19 23:56:12 +0300

    s21_div.c has been created.

--- src/s21_decimal.h
+++ src/s21_decimal.h
@@ -5,6 +5,13 @@
 #define OK 0
 #define CONVERT_ERROR 1
 
+#define TOO_BIG 1
+#define TOO_LITTLE 2
+#define DIV_BY_ZERO 3
+
+#define FALSE 0
+#define TRUE 1
+
 typedef struct {
   int bits[4];
 } s21_decimal;
@@ -17,5 +24,27 @@ int s21_from_float_to_decimal(float src, s21_decimal *dst);
 int s21_from_decimal_to_int(s21_decimal src, int *dst);
 // В float
 int s21_from_decimal_to_float(s21_decimal src, float *dst);
+
+// Сложение
+int s21_add(s21_decimal value_1, s21_decimal value_2, s21_decimal *result);
+// Вычитание
+int s21_sub(s21_decimal value_1, s21_decimal value_2, s21_decimal *result);
+// Умножение
+int s21_mul(s21_decimal value_1, s21_decimal value_2, s21_decimal *result);
+// Деление
+int s21_div(s21_decimal value_1, s21_decimal value_2, s21_decimal *result);
+
+// Меньше
+int s21_is_less(s21_decimal, s21_decimal);
+// Меньше или равно
+int s21_is_less_or_equal(s21_decimal, s21_decimal);
+// Больше
+int s21_is_greater(s21_decimal, s21_decimal);
+// Больше или равно
+int s21_is_greater_or_equal(s21_decimal, s21_decimal);
+// Равно
+int s21_is_equal(s21_decimal, s21_decimal);
+// Не равно
+int s21_is_not_equal(s21_decimal, s21_decimal);
 
 #endif  //  SRC_S21_DECIMAL_H_
---
+++ src/s21_div.c
@@ -0,0 +1,18 @@
+#include "s21_decimal.h"
+
+
+int s21_div(s21_decimal value_1, s21_decimal value_2, s21_decimal *result) {
+    int res = OK;
+    
+
+
+
+    if() {
+        res = TOO_BIG;
+    } else if() {
+        res = TOO_LITTLE;
+    } else if() {
+        res = DIV_BY_ZERO;
+    }
+    return res;
+}
